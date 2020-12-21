d98694eb02be58fdc4d10a297e822a7e9bc77409
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-12-22 01:23:55 +0800

    Before putting dummy html to components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductListModule } from './product-list/product-list.module';
 import { ProductDetailsModule } from './product-details/product-details.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
